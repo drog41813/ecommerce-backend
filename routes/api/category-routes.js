2989d0a66e3769588bcecc35cc37d166f3705a4b
drog41813 <daniel.lee042003@gmail.com>
2023-08-09 18:18:41 -0500

    added req.body within the creates

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,7 +33,7 @@ router.get('/:id', async (req, res) => {
 });
 router.post('/', async (req, res) => {
   try{
-    const categoryData = await Category.create();
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   }
   catch (err){
@@ -43,7 +43,11 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try{
-    const categoryData = await Category.update(req.body);
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
     res.status(200).json(categoryData);
   }
   catch (err){
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,7 +34,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try{
-    const tagData = await Tag.create();
+    const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
   }
   catch (err){
